82f7f058aeddd80f65c2dbf53edfdf54e59d451e
Yu Sun <yu.sun.cs@gmail.com>
2021-08-25 17:16:13 -0700

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "99aba6b26eab6556437e28c256902852",
-"index.html": "3bfdb58fadcfe08908bb248871bb47b1",
-"/": "3bfdb58fadcfe08908bb248871bb47b1",
-"main.dart.js": "665605df1da623524d1d25e3ed3b089a",
+"index.html": "db46326f87361aaf44c52c8512fff937",
+"/": "db46326f87361aaf44c52c8512fff937",
+"main.dart.js": "981130a3c2b1af0e929c709b128f63de",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
