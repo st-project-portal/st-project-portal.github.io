217df35026c88a319923399770d638bbee66d385
angli <18629692129la@gmail.com>
2021-12-01 11:27:49 -0800

    update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -4,9 +4,9 @@ const TEMP = 'flutter-temp-cache';
 const CACHE_NAME = 'flutter-app-cache';
 const RESOURCES = {
   "version.json": "99aba6b26eab6556437e28c256902852",
-"index.html": "2ccf5b384c93d4fe6924779be78ed647",
-"/": "2ccf5b384c93d4fe6924779be78ed647",
-"main.dart.js": "4cc11ddafd5d58dbd785c8898982e881",
+"index.html": "aa4c07af2c27b74b2d009adcec1f3311",
+"/": "aa4c07af2c27b74b2d009adcec1f3311",
+"main.dart.js": "b43b24c89c208db37f91b34d61bbc918",
 "favicon.png": "5dcef449791fa27946b3d35ad8803796",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
