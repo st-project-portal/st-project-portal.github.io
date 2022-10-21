b50c9ec902d5c9f09944702a34a80fbcc25eab07
hypnogod <sanskarpok11@gmail.com>
2022-10-21 09:52:13 -0400

    Update

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -16,9 +16,9 @@ const RESOURCES = {
 "flutter.js": "eb2682e33f25cd8f1fc59011497c35f8",
 "icons/Icon-192.png": "ac9a721a12bbc803b44f645561ecb1e1",
 "icons/Icon-512.png": "96e752610906ba2a93c65f8abe1645f1",
-"index.html": "a9a7ad69d3417e2032349797f48b28da",
-"/": "a9a7ad69d3417e2032349797f48b28da",
-"main.dart.js": "0985f31be13b61805fe2158c73eaa5f4",
+"index.html": "75027f84b6da5904428615a39cf426c0",
+"/": "75027f84b6da5904428615a39cf426c0",
+"main.dart.js": "d424c38740e46efaf3210fbb41d4a4b2",
 "manifest.json": "48e0f55e12d6dc65f78d9e51bdaccb8a",
 "version.json": "363e4d7c718d14f66fae78841f02c645"
 };
